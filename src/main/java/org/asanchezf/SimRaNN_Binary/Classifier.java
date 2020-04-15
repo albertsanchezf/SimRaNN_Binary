5b1bbe7d47e4bc012b042ccc8eff317ac0f01d6c
albertsanchezf <asanchezfuster@gmail.com>
2020-04-15 19:06:36 +0200

    Update version

--- src/main/java/org/asanchezf/SimRaNN_Binary/Classifier.java
+++ src/main/java/org/asanchezf/SimRaNN_Binary/Classifier.java
@@ -21,6 +21,7 @@ import org.nd4j.evaluation.classification.Evaluation;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.SplitTestAndTrain;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
@@ -48,24 +49,13 @@ public class Classifier {
         int numLinesToSkip = 0;
         char delimiter = ',';
 
-        //String datasetPath = "/Users/AlbertSanchez/Desktop/Post/DatasetStatistics_v1/1/dataset.csv"; //DS
-        String datasetTrain = "/Users/AlbertSanchez/Desktop/Post/DatasetStatistics500/3/dataset1_train.csv"; //DS
-        String datasetTest = "/Users/AlbertSanchez/Desktop/Post/DatasetStatistics500/3/dataset1_test.csv"; //DS
-
-        //File earlyStoppingModelFile = new File("resources/earlyStoppingModels/DSNet.zip");
+        String datasetPath = "/Users/AlbertSanchez/Desktop/Post/DatasetStatistics500/1/dataset.csv"; //DS
 
         int numClasses = 1;  //1 class (types of incidents). 0 - No incident | 1 - Incident
         int batchSize = 512;
 
-        //RecordReader recordReader = new CSVRecordReader(numLinesToSkip,delimiter);
-        //recordReader.initialize(new FileSplit(new File(datasetPath)));
-
-        // Load Train DS
-        RecordReader recordReader1 = new CSVRecordReader(numLinesToSkip,delimiter);
-        recordReader1.initialize(new FileSplit(new File(datasetTrain)));
-        // Load Test DS
-        RecordReader recordReader2 = new CSVRecordReader(numLinesToSkip,delimiter);
-        recordReader2.initialize(new FileSplit(new File(datasetTest)));
+        RecordReader recordReader = new CSVRecordReader(numLinesToSkip,delimiter);
+        recordReader.initialize(new FileSplit(new File(datasetPath)));
 
         // Build a Input Schema
         Schema inputDataSchema = new Schema.Builder()
@@ -88,7 +78,6 @@ public class Classifier {
         //Second: the RecordReaderDataSetIterator handles conversion to DataSet objects, ready for use in neural network
         int labelIndex = outputSchema.getColumnNames().size() - 1;     //15 values in each row of the dataset.csv; CSV: 14 input features followed by an integer label (class) index. Labels are the 15th value (index 14) in each row
 
-        /*
         TransformProcessRecordReader transformProcessRecordReader = new TransformProcessRecordReader(recordReader,tp);
         DataSetIterator iterator = new RecordReaderDataSetIterator(transformProcessRecordReader,batchSize,labelIndex,numClasses);
         DataSet allData = iterator.next();
@@ -97,32 +86,20 @@ public class Classifier {
 
         DataSet trainingData = testAndTrain.getTrain();
         DataSet testData = testAndTrain.getTest();
-        */
-
-        TransformProcessRecordReader transformProcessRecordReader1 = new TransformProcessRecordReader(recordReader1,tp);
-        TransformProcessRecordReader transformProcessRecordReader2 = new TransformProcessRecordReader(recordReader2,tp);
-        DataSetIterator trainingData = new RecordReaderDataSetIterator(transformProcessRecordReader1,batchSize,labelIndex,numClasses);
-        DataSetIterator testData = new RecordReaderDataSetIterator(transformProcessRecordReader2,batchSize,labelIndex,numClasses);
-
 
         //We need to normalize our data. We'll use NormalizeStandardize (which gives us mean 0, unit variance):
         DataNormalization normalizer = new NormalizerStandardize();
         normalizer.fit(trainingData);           //Collect the statistics (mean/stdev) from the training data. This does not modify the input data
 
-        while(trainingData.hasNext())
-            normalizer.transform(trainingData.next());     //Apply normalization to the training data
-        while (testData.hasNext())
-            normalizer.transform(testData.next());         //Apply normalization to the test data. This is using statistics calculated from the *training* set
-
-        trainingData.reset();
-        testData.reset();
+        normalizer.transform(trainingData);     //Apply normalization to the training data
+        normalizer.transform(testData);         //Apply normalization to the test data. This is using statistics calculated from the *training* set
 
         log.info("Build model....");
         MultiLayerConfiguration conf;
         conf = new NeuralNetConfiguration.Builder()
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .l2(1e-4)
-                .updater(new Sgd(0.01))
+                .updater(new Sgd(0.1))
                 .list()
                 .layer(new DenseLayer.Builder()
                         .nIn(labelIndex)
@@ -161,61 +138,21 @@ public class Classifier {
         model.init();
         model.setListeners(new ScoreIterationListener(100));
 
-        /* EARLY STOPPING */
-        /*
-        EarlyStoppingConfiguration esConf = new EarlyStoppingConfiguration.Builder()
-                .epochTerminationConditions(new MaxEpochsTerminationCondition(30))
-                .iterationTerminationConditions(new MaxTimeIterationTerminationCondition(20, TimeUnit.MINUTES))
-                .scoreCalculator(new DataSetLossCalculator(testData, true))
-                .evaluateEveryNEpochs(1)
-                .modelSaver(new LocalFileModelSaver(earlyStoppingModelFile))
-                .build();
-
-        EarlyStoppingTrainer trainer = new EarlyStoppingTrainer(esConf,model,trainData);
-
-        EarlyStoppingResult result = trainer.fit();
-
-        //Print out the results:
-        System.out.println("Termination reason: " + result.getTerminationReason());
-        System.out.println("Termination details: " + result.getTerminationDetails());
-        System.out.println("Total epochs: " + result.getTotalEpochs());
-        System.out.println("Best epoch number: " + result.getBestModelEpoch());
-        System.out.println("Score at best epoch: " + result.getBestModelScore());
-
-        MultiLayerNetwork bestModel = result.getBestModel();
-        */
-        /* END EARLY STOPPING SECTION */
-
         for(int i=0; i<5000; i++) {
             model.fit(trainingData);
         }
 
         // Evaluate the model on the test set
-        /*
         Evaluation eval = new Evaluation(numClasses);
         INDArray output = model.output(testData.getFeatures());
         eval.eval(testData.getLabels(), output);
         log.info(eval.stats(true));
         System.out.println(eval.stats());
-        */
-
-        Evaluation eval = new Evaluation(numClasses);
-        DataSet nextTestData;
-        while(testData.hasNext())
-        {
-            nextTestData = testData.next();
-            INDArray output = model.output(nextTestData.getFeatures());
-            eval.eval(nextTestData.getLabels(),output);
-        }
-        log.info(eval.stats(true));
-        //System.out.println(eval.stats());
 
         // Save the trained model
-        renameDSNetLastFile(numClasses);
         File locationToSave; //Where to save the network. Note: the file is in .zip format - can be opened externally
 
-        //locationToSave = new File("resources/trainedNN/binaryDSNet_DS.zip"); //DS
-        locationToSave = new File("resources/trainedNN/DSNet.zip"); //512
+        locationToSave = new File(obtainFilename(System.getProperty("user.dir") + "/resources/","DSNet"));
 
         ModelSerializer.writeModel(model,locationToSave, false);
         System.out.println("Model trained saved in: " + locationToSave.toString());
@@ -226,58 +163,45 @@ public class Classifier {
 
     }
 
-    public static void renameDSNetLastFile(int numClasses)
+    public static String obtainFilename(String path, String name)
     {
-        File f;
         String[] s, s1;
-        String filename, path = System.getProperty("user.dir") + "/resources/trainedNN/";
+        String filename = "";
         boolean dsFile = false;
         int maxFile = 0;
-        filename = "DSNet"; //All
 
-        f = new File(path + filename + ".zip");
-
-        if (f.exists())
+        // To ensure any file is overwrited
+        File f = new File(path);
+        String[] files = f.list();
+        for(int i=0; i < files.length; i++)
         {
-            f = new File(path);
-            // To ensure any file is overwritten
-            String[] files = f.list();
-            for (String file : files)
+            if(new File(path + files[i]).isFile())
             {
-                if(new File(path+file).isFile())
+                s = files[i].split(name);
+                if(s.length==2)
                 {
-                    s = file.split(filename+"_");
-                    if(s.length==2)
+                    if(s[1].equals(".zip"))
+                        dsFile = true;
+                    else
                     {
-                        if(!s[1].equals(".zip"))
-                        {
-                            s1 = s[1].split(".zip");
-                            if (s1[0].matches("^[0-9]*$"))
-                                if(Integer.valueOf(s1[0]) > maxFile) maxFile = Integer.valueOf(s1[0]);
-                        }
+                        s1 = s[1].split(".zip");
+                        if (s1[0].matches("^[0-9]*$"))
+                            if(Integer.valueOf(s1[0]) > maxFile)
+                                maxFile = Integer.valueOf(s1[0]);
                     }
                 }
             }
-            maxFile++;
-            new File(path + filename + ".zip").renameTo(new File(path + filename + "_" + String.valueOf(maxFile) + ".zip")); //All
-
         }
 
+        maxFile++;
+
+        filename = path + name + maxFile + ".zip";
+
+        return filename;
     }
 
-    private static String textfile2String(String filePath)
-    {
-        StringBuilder contentBuilder = new StringBuilder();
-        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.US_ASCII))
-        {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        return contentBuilder.toString();
-    }
+
+
 
 }
 
